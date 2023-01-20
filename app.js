f944f7b2fbc5d2499478b09a1f9f5a43d5d2a74f
Ivan T <112672027+outstandingly64@users.noreply.github.com>
2023-01-20 15:19:43 -0500

    dotenv import

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+//middleware imports
 import errorHandler from './middleware/error-handler.js';
 import notFoundHandler from './middleware/not-found.js';
-const app = express();
 
 app.get('/', (req, res) => {
     res.send(`Hello Vocation Village!`);
